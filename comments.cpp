6bf9212e9a3f5fcc605197e91d7990d7d70d98a9
Pranav Singh <138393493+Pranavsingh431@users.noreply.github.com>
2023-07-03 13:50:14 +0530

    learning comments in c++

---
+++ comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>//this brings in the iostream library.
+using namespace std;//this is used to run a code without using std::
+int main(){//it is the main function in which we write our code.
+  //this is single line comment
+   cout << "number1" << endl;//endl to end the line and start another line from the next line
+   cout << "number2" << endl;
+   cout << "number3" << endl;//everyline ending with a semicolon is called statement.
+/*
+this is a multiline comment also know as (block comment).
+like this
+and this.
+std stands for standard maybe means standard syntax to write a code in c++.
+*/
+   return 0;
+
+}
